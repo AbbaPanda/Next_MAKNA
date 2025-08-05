e587614870a6a0c7251835fb44dce0064ab8b3e8
AbbaPanda <aryandagp50@gmail.com>
2025-08-05 08:49:46 +0700

    happy weekday

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { Poppins } from "next/font/google";
- 
- export default function Home() {
+import { Button } from '@/components/ui/button';
+import { Poppins } from 'next/font/google';
+
+export default function Home() {
   return (
-    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+    <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
       <h1> MAKNA </h1>
       <Button>Transform Now</Button>
+      <h1 className='flex justify-center gap-3 bg-amber-600'>
+        menjadi lebih baik dari dalam
+      </h1>
     </div>
   );
 }
