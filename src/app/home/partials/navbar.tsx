f4eecb3b81fa565176767b2553c0234f6eec8f1f
AbbaPanda <aryandagp50@gmail.com>
2025-08-09 23:40:49 +0700

    mulai burger

--- src/app/home/partials/navbar.tsx
+++ src/app/home/partials/navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { navigationData } from '@/app/constants/navigation-data';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { Sheet, SheetTrigger } from '@/components/ui/sheet';
 
 const Navbar = () => {
 
@@ -12,7 +13,7 @@ const Navbar = () => {
   const background = useTransform(
     scrollY,
     [0, 100],
-    ['rgba(0, 0, 0, 0)', 'rgba(0,0,0,0.8)']
+    ['rgba(0, 0, 0, 0)', 'rgba(0,0,0,0.5)']
   );
 
   const backdropBlur = useTransform(
@@ -23,7 +24,7 @@ const Navbar = () => {
 
   return (
     <motion.header style={{ background, backdropFilter: backdropBlur }}
-      className='fixed z-50 w-full top-0'>
+      className='fixed items-center z-50 w-full top-0'>
       <div className='custom-container flex-between h-16 px-4 md:h-21'>
         <div className='flex items-center gap-2'>
           <Image
@@ -38,15 +39,20 @@ const Navbar = () => {
           <ul className='flex items-center gap-5 '>
             {navigationData.map((data) => (
               <li key={data.title}>
-                <Link className='md:font-semibold md:text-md font-semibold hover:text-primary-200 hover:shadow-amber-300 active:text-primary-200'
+                <Link className='md:font-semibold md:text-md font-semibold hover:text-primary-200 hover:shadow-amber-300 active:text-primary-200 active:shadow-amber-300 transition-all duration-200'
                   href={data.href}>{data.title}</Link>
               </li>
             ))}
           </ul>
         </nav>
-        <Button className='text-sm font-semibold w-30'>
+        <Button className='hidden md:flex text-sm font-semibold'>
           <Link href='/contact'>Mulai Disini</Link>
         </Button>
+        <Sheet>
+          <SheetTrigger>
+
+          </SheetTrigger>
+        </Sheet>
       </div>
     </motion.header >
   );
