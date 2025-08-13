9a8c2b10118e2c9c18ec1045d7fc4cb3e40351b1
AbbaPanda <aryandagp50@gmail.com>
2025-08-13 16:11:42 +0700

    hover working

--- src/assets/components/layout/header.tsx
+++ src/assets/components/layout/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { HeaderProps, NavItems } from '../types/header';
 import makna from '../../images/LOGO-MAKNA.png';
 import hamburger from '../../images/hamburger.png';
+import Image from 'next/image';
 
 const Header: React.FC<HeaderProps> = ({ navItems }) => {
   return (
@@ -9,7 +10,7 @@ const Header: React.FC<HeaderProps> = ({ navItems }) => {
     <div className='w-full flex justify-center sm:px-0 mt-5 z-30 absolute top-0 left-0 right-0 text-white'>
       {/* // wrapper logo */}
       <div className='left-4 absolute top-0 flex-wrap'>
-        <img src={makna} alt='makna' className='w-40' />
+        <Image src={makna} alt='makna' width={120} height={120} className='w-40' />
       </div>
       {/* wrapper navItems */}
       <nav className='right-4 absolute top-0 text-sm font-semibold justify-evenly hidden sm:flex gap-7'>
@@ -20,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ navItems }) => {
         ))}
       </nav>
       <button className='right-4 absolute top-0 sm:hidden flex items-center justify-center'>
-        <img src={hamburger} alt='hamburger' />
+        <Image src={hamburger} alt='hamburger' width={120} height={120} />
       </button>
     </div>
   );
