12e388a5630045e21864d7804ba2142cb6be1d5b
AbbaPanda <aryandagp50@gmail.com>
2025-08-13 15:32:31 +0700

    hover working

---
+++ src/app/home/partials/hero.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@radix-ui/themes'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+
+      {/* left */}
+      <div>
+        <div>
+          <h1>MENCIPTA<span>Perlaku Sukses</span></h1>
+
+          <p>deskripsi</p>
+          <Button />
+        </div>
+      </div>
+      {/* right */}
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
