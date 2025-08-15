0f63eff3201c314c0eb1b0d61c60468528094bd0
AbbaPanda <aryandagp50@gmail.com>
2025-08-15 16:55:17 +0700

    finishing hero

--- src/app/home/partials/hero.tsx
+++ src/app/home/partials/hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/assets/components/ui/button'
-import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
@@ -7,18 +7,20 @@ const Hero = () => {
     <section className='custom-container flex flex-wrap items-center gap-7 overflow-hidden md:pt-57.5 pt-31.25'>
 
       {/* left */}
-      <div className='flex-[4.4] basis-80 z-20'>
+      <div className='flex-[4.4] basis-80 z-20 pb-9 '>
         <div className='flex max-w-[653px] flex-col gap-10'>
-          <h1 className='text-display-lg text-neutral-25 md:text-display-3xl leading-tight font-semibold md:font-bold'>MENCIPTA <br /><span className='text-primary-200'> Perlaku Sukses</span></h1>
+          <h1 className='text-display-lg text-neutral-50 md:text-display-3xl leading-tight font-semibold md:font-bold'>MENCIPTA <br /><span className='text-primary-200'> Perlaku Sukses</span></h1>
 
-          <p>deskripsi</p>
-          <Button>Mulai</Button>
+          <p className='text-md font-semibold md:text-xl md:font-semibold'>deskripsi</p>
+          <Button asChild className='flex h-11 w-full px-16 md:h-12 md:w-fit'>
+            <Link href='#contact'>Mulai Disini</Link>
+          </Button>
         </div>
       </div>
       {/* right */}
-      <div className='flex-[5.6] basis-80'>
+      <div className='flex-[5.6] basis-80 md:absolute '>
         <div>
-          <Image src="" alt="bg" />
+          <img src='/images/hero-image.png' alt="bg" />
         </div>
       </div>
     </section>
